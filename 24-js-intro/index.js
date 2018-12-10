dfc868fe320dba35c66c0be065fcb494148d3e92
andrew cohn <andrewcohn0@gmail.com>
2018-12-10 10:29:50 -0500

    js intro

--- 24-js-intro/index.js
+++ 24-js-intro/index.js
@@ -1,1 +1,48 @@
 console.log('%c HELLO WORLD', 'color: green')
+console.log('%c HELLO WORLD', 'color: gold')
+console.log('%c HELLO WORLD', 'color: firebrick')
+
+let x = 1
+
+let y = x
+
+y++
+
+console.log('X IS, ', x)
+console.log('Y IS, ', y)
+
+function beefy() {
+  console.log('I am a beefy fn')
+}
+
+console.log(beefy)
+
+beefy()
+
+const tasty = function() {
+  console.log('super tasty')
+}
+
+tasty.quality = 'high'
+
+// debugger
+
+console.dir(tasty)
+
+
+function addOne(num) {
+  return num + 1
+}
+
+
+const nums = [1, 2, 3].map(addOne)
+
+
+const names = ['evans', 'also evans', 'vicky', 'steven'].map(function mapNames(name) {
+  return name.toUpperCase()
+})
+
+
+if (true) {
+  console.log('THIS IS TRUE')
+}
