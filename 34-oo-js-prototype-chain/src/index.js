e18783392fdcbbf1b0fb37ea75455d2cd5596154
andrew cohn <andrewcohn0@gmail.com>
2018-12-18 16:11:23 -0500

    add arrow keys

--- 34-oo-js-prototype-chain/src/index.js
+++ 34-oo-js-prototype-chain/src/index.js
@@ -6,11 +6,17 @@ document.body.addEventListener('click', link.stop.bind(link))
 function handleKeyDown(e) {
   console.log(this)
   switch (e.keyCode) {
+    case 37:
+      link.walkWest()
+      break
     case 39:
       link.walkEast()
       break
     case 40:
       link.walkSouth()
+      break
+    case 38:
+      link.walkNorth()
       break
     case 32:
       link.stop()
